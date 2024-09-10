3f7abcf5b6ba599441e4b30b56f49a09b13d1f7b
JoaoVitorMT <matiasjoaovitor03@gmail.com>
2024-09-10 09:17:27 +0100

    Add files via upload

---
+++ Lista1/ipva.java
@@ -0,0 +1,19 @@
+package lista13Exercícios;
+import java.util.Scanner;
+
+public class ipva {
+	public static void main  (String args[]) {
+	Scanner ler = new Scanner(System.in);
+	
+	double valorcarro, ipva;
+
+	System.out.println("Digite o valor do carro :");
+	valorcarro = ler.nextInt();
+	
+	ipva = (valorcarro/100) * 4;
+	
+	System.out.println("O valor do IPVA é : " + ipva );
+	ler.close();
+
+}
+}
