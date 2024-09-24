3f84787b939e02cb9fe6e81fb7945b62988f9ed5
JoaoVitorMT <matiasjoaovitor03@gmail.com>
2024-09-24 09:20:08 -0300

    Add files via upload

---
+++ For/multiplo.java
@@ -0,0 +1,21 @@
+package exfor;
+
+public class multiplo {
+	public static void main(String[] args) {
+		int  i   ;
+		
+		for(  i = 1; i <= 100; i ++)
+			
+			if (i%10!=0){
+				System.out.println(i);
+				
+				
+			}else {
+				
+				System.out.println(i+ " é  multiplo de 10 ");
+				
+			}
+			
+	
+	}
+}
